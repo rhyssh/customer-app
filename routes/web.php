013530a8b75a67e8a203b1cf6e7711b5a0ef5bef
rhyssh <reikiaziz30@gmail.com>
2025-02-13 16:37:22 +0700

    fix: error route force destroy

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('customer.index');
+    return view('welcome');
 });
 Route::get('customer/trash', [CustomerController::class, 'trashIndex'])->name('customer.trash'); 
 Route::get('customer/reestore/{customer}', [CustomerController::class, 'reestoreIndex'])->name('customer.restore');
-Route::get('customer/trash.{customer}', [CustomerController::class, 'forceDestroy'])->name('customer.forceDestroy');
+Route::delete('customer/trash/{customer}', [CustomerController::class, 'forceDestroy'])->name('customer.forceDestroy');
 Route::resource('customer', CustomerController::class);
